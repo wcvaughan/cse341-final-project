0ef00583119fcf74e549e895ff46ad0ab3d86b73
enochbalbuena <enxch@icloud.com>
2025-03-31 16:42:52 -0600

    changed callback URL

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -15,7 +15,7 @@ passport.deserializeUser((obj, done) => {
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/callback"
+  callbackURL: "https://cse341-node-r714.onrender.com/auth/callback"
 },
 (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
